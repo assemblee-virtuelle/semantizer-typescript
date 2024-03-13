7050498b1863b0595fa7d6becdce828d99d875fb
Maxime Lecoq <maxime@lecoqlibre.fr>
2024-03-13 15:26:44 +0100

    Update lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,8 @@
-export { default as Semanticable } from './Semanticable';
+export { default as ISemantizer } from './ISemantizer.js';
+export { default as IContext } from './IContext.js';
+export { default as Semanticable } from './Semanticable.js';
+export { default as Semantizer } from './Semantizer.js';
+export { default as Context } from './Context.js';
 export { default as SemanticObject } from './SemanticObject.js';
 export { default as SemanticObjectAnonymous } from './SemanticObjectAnonymous.js';
 //# sourceMappingURL=index.d.ts.map
