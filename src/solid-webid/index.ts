8e9349d25f0147742131c9ed4392cbb88e151f18
Maxime Lecoq <maxime@lecoqlibre.fr>
2024-07-22 16:20:44 +0200

    Add package for Solid WebId Profile

---
+++ src/solid-webid/index.ts
@@ -0,0 +1,36 @@
+import { Document, Thing } from "@semantizer/types";
+import { WebIdProfileConstructor } from "@semantizer/webid";
+
+export interface SolidWebIdProfileNonDestructiveOperations {
+    getPublicTypeIndexUri(): string | undefined;
+}
+
+export type SolidWebIdProfile = Document<Thing, Thing> & SolidWebIdProfileNonDestructiveOperations;
+export type SolidWebIdProfileConstructor = new (...args: any[]) => SolidWebIdProfile;
+
+const context = {
+    solid: "http://www.w3.org/ns/solid/terms#",
+    pim: "http://www.w3.org/ns/pim/space#",
+    ldp: "http://www.w3.org/ns/ldp#",
+    publicTypeIndex: "solid:publicTypeIndex"
+}
+
+export function SolidWebIdProfileMixin<
+    TBase extends WebIdProfileConstructor
+>(Base: TBase) {
+    return class SolidWebIdProfileImpl extends Base implements SolidWebIdProfile {
+
+        public constructor(...args: any[]) {
+            super(...args);
+        }
+
+        public getPublicTypeIndexUri(): string | undefined {
+            const primaryTopic = this.getPrimaryTopic();
+            return primaryTopic.getStatement(context.solid + "publicTypeIndex")?.getValue();
+        }
+
+    }
+
+}
+
+export default SolidWebIdProfileMixin;
