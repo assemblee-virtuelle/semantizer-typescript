ffb6666eb36890c435d2fec1af1f5faa92563997
Maxime Lecoq <maxime@lecoqlibre.fr>
2024-09-06 17:32:30 +0200

    Remove core-rdfjs old files

--- src/core-rdfjs/old/RdfjsDatasetSerializable.ts
+++
@@ -1,7 +0,0 @@
-import DatasetCore from "@rdfjs/dataset/DatasetCore";
-
-export interface RdfjsDatasetSerializable {
-    toRdfjsDataset(): DatasetCore;
-}
-
-export default RdfjsDatasetSerializable;
